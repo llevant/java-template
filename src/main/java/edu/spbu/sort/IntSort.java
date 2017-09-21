af428a42b750d8a55ae598fea77347667854f979
Elizaveta Trusova <liz9299@mail.ru>
2017-09-21 11:34:16 +0300

    Merge Sort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -4,13 +4,90 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Created by artemaliev on 07/09/15.
- */
+
+
 public class IntSort {
-  
+
+  // Слияние двух подмассивов arr[l..m] и arr[m+1..r] массива arr[]
+
+  static void merge(int arr[], int l, int m, int r)
+  {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+        /* временные массивы */
+    int L[] = new int [n1];
+    int R[] = new int [n2];
+
+    for (int i=0; i<n1; ++i)
+      L[i] = arr[l + i];
+    for (int j=0; j<n2; ++j)
+      R[j] = arr[m + 1+ j];
+
+
+        /* сливаем временные массивы */
+    int i = 0, j = 0;
+    int k = l;
+    while (i < n1 && j < n2)
+    {
+      if (L[i] <= R[j])
+      {
+        arr[k] = L[i];
+        i++;
+      }
+      else
+      {
+        arr[k] = R[j];
+        j++;
+      }
+      k++;
+    }
+
+        /* копир оставшиеся элементы L[] */
+    while (i < n1)
+    {
+      arr[k] = L[i];
+      i++;
+      k++;
+    }
+
+        /* копир оставшиеся элементы R[] */
+    while (j < n2)
+    {
+      arr[k] = R[j];
+      j++;
+      k++;
+    }
+  }
+
+  // рекурс функция сортировки массива arr[l..r]
+  static void sort(int arr[], int l, int r)
+  {
+    if (l < r)
+    {
+      int m = (l+r)/2;
+      // сортируем половинки
+      sort(arr, l, m);
+      sort(arr ,m+1, r);
+
+      // и сливаем их
+      merge(arr, l, m, r);
+    }
+  }
+
+  // вывод
+  static void printArray(int arr[])
+  {
+    int n = arr.length;
+    for (int i=0; i<n; ++i)
+      System.out.print(arr[i] + " ");
+    System.out.println();
+  }
+
   public static void sort (int array[]) {
-    Arrays.sort(array);
+
+    sort(array, 0, array.length-1);
+
   }
 
   public static void sort (List<Integer> list) {
