35032829580cdc37f02564c2517bb6f9ce502152
Llevante <liz9299@mail.ru>
2017-11-30 13:00:32 +0300

    dense+sparse mul+dmul

--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -16,4 +16,45 @@ public class MatrixTest
     Matrix expected = new DenseMatrix("result.txt");
     assertEquals(expected, m1.mul(m2));
   }
+
+  @Test
+  public void mulDS() {
+    Matrix m1 = new DenseMatrix("m1.txt");
+    Matrix m2 = new SparseMatrix("m2.txt");
+    Matrix expected = new DenseMatrix("result.txt");
+    assertEquals(expected, m1.mul(m2));
+  }
+
+  @Test
+  public void mulSD() {
+    Matrix m1 = new SparseMatrix("m1.txt");
+    Matrix m2 = new DenseMatrix("m2.txt");
+    Matrix expected = new DenseMatrix("result.txt");
+    assertEquals(expected, m1.mul(m2));
+  }
+
+  @Test
+  public void mulSS() {
+    Matrix m1 = new SparseMatrix("m1.txt");
+    Matrix m2 = new SparseMatrix("m2.txt");
+    Matrix expected = new SparseMatrix("result.txt");
+    assertEquals(expected, m1.mul(m2));
+  }
+
+  @Test
+  public void dmulDD() {
+    Matrix m1 = new DenseMatrix("m1.txt");
+    Matrix m2 = new DenseMatrix("m2.txt");
+    Matrix expected = new DenseMatrix("result.txt");
+    assertEquals(expected, m1.dmul(m2));
+  }
+
+  @Test
+  public void dmulSS() {
+    Matrix m1 = new SparseMatrix("m1.txt");
+    Matrix m2 = new SparseMatrix("m2.txt");
+    Matrix expected = new SparseMatrix("result.txt");
+    Matrix m3 = m1.dmul(m2);
+    assertEquals(expected, m3);
+  }
 }
